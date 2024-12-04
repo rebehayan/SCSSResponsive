51fadbd05406a9796c367c88e7038aead9109d1b
rebehayan <rebehayan@gmail.com>
2024-12-05 08:20:18 +0900

    fix:ios

---
+++ js/ios.js
@@ -0,0 +1,15 @@
+const userAgent = navigator.userAgent;
+let version = null;
+
+if (/iP(hone|od|ad)/.test(userAgent)) {
+  // iOS User-Agent에서 버전 추출
+  const match = userAgent.match(/OS (\d+_\d+(_\d+)?)/);
+  if (match && match[1]) {
+    version = match[1].replace(/_/g, "."); // iOS 버전을 '.'으로 구분하여 반환
+  }
+}
+
+if (version) {
+  alert("ios입니다.");
+  document.body.classList.add("ios");
+}
